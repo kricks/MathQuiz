d59501316026f2898b64e9860bd52bccb940bcf2
Katie Ricks <ric12052@byui.edu>
2017-12-12 00:34:01 -0700

    upload

--- Math Quiz/Program.cs
+++ Math Quiz/Program.cs
@@ -16,7 +16,7 @@ namespace Math_Quiz
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MathQuiz());
         }
     }
 }
